32cbc4a43bfcbf94423f775d3e714450dd550adf
Bryant <bryly27@sbcglobal.net>
2015-04-07 00:19:13 -0700

    added favicon

--- application/views/main.php
+++ application/views/main.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title>Pawstumes</title>
+<link rel="shortcut icon" href="/assets/img/favicon.png" />
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
@@ -25,6 +26,6 @@
 				</div>
 <?php	} ?>
 	</div>
-	
+
 </body>
 </html>
