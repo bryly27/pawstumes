3d14521a32332b2cda30e76631df775ae4a6655f
Bryant <bryly27@sbcglobal.net>
2015-04-07 22:32:20 -0700

    changed titles

--- application/views/admin_home.php
+++ application/views/admin_home.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Pawstumes</title>
+	<title>Admin</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
