87cc0400aa1fa15229a3ab0816e2c3f12dd53c13
Bryant <bryly27@sbcglobal.net>
2015-04-13 02:25:28 -0700

    changed h1

--- application/views/main.php
+++ application/views/main.php
@@ -14,7 +14,7 @@
 <body>	
 	<div class='row'>
 		<div class='col-md-12 col-xs-12 header container'>
-			<h1>Pawstumes</h1>
+			<h1>Pawstumes | Pets in Costume</h1>
 		</div>
 	</div>
 	<div class='pet_photos'>
