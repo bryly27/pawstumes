6ef90b7eef3e2751f9cd6f3b667321c644089162
Bryant <bryly27@sbcglobal.net>
2015-05-31 04:14:14 -0700

    adding adsense

--- application/views/main.php
+++ application/views/main.php
@@ -18,6 +18,16 @@
 		</div>
 	</div>
 
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- pawstumes_ad1 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-6439372639315752"
+     data-ad-slot="3551918823"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 	<div class='pet_photos'>
 
 <?php  
