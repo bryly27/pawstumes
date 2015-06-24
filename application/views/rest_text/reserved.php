42091459ecb3b5c2575131a24a0de30425a5ca81
Bryant <bryly27@sbcglobal.net>
2015-06-24 01:51:19 -0700

    test

--- application/views/rest_text/reserved.php
+++ application/views/rest_text/reserved.php
@@ -1,4 +1,9 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php  
+	header('Content-type: text/xml');
+   echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
 <Response>
   <Say voice='alice'>This is a message from restaurant text. We will notify you when your table is ready. Good bye. </Say>
 </Response>
+	
